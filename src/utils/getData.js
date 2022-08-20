fb2793fde140c72c9a3ff218d090f8161115e039
dhereira <davidhereira@gmail.com>
2022-08-20 15:22:36 -0500

    regreso todo los cambio hasta llegar al original del curso

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -4,7 +4,7 @@ const API = process.env.API;
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
-    const response = await fetch(apiURl, {method: 'GET', mode:'cors', header:{origin: 'https://63013f05000f7d00087657c1--magenta-sable-90c232.netlify.app/'}});
+    const response = await fetch(apiURl);
     const data = await response.json();
     return data.results[0];
   } catch (error) {
