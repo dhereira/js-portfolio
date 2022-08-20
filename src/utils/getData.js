d80efe8d05f3b71d5a373d6bd3b6fb19a64ec1eb
dhereira <davidhereira@gmail.com>
2022-08-20 15:07:15 -0500

    sigo probando opciones en fetch

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,9 +1,10 @@
 const API = process.env.API;
 
+
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
-    const response = await fetch(apiURl,{mode:'no-cors'});
+    const response = await fetch(apiURl, {method: 'GET', mode:'cors', header:{origin: 'apiURL'}});
     const data = await response.json();
     return data.results[0];
   } catch (error) {
